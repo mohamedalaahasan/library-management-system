c0e0774161b7500985f1739735a8b005fc3fc576
Mohamed Alaa <mohamedalaahasan309@gmail.com>
2026-08-30 19:34:57 +0300

    Add user authentication

--- users/views.py
+++ users/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import SignUpForm
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('book_list')
+
+    else:
+        form = SignUpForm()
+
+    return render(request, 'users/signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect('book_list')
+
+    return render(request, 'users/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
